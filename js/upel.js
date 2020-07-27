b6a6d6f2900b31f754c7eb2681f16c5a893834e7
Sotirisgkr <sotirisgkr@hotmail.com>
2020-07-27 19:47:25 +0300

    animations

--- js/upel.js
+++ js/upel.js
@@ -1,17 +1,25 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+$(document).ready(function(){
+    //Add smooth scrolling to all links
+    $("a").on('click',function(event){
+        //Make sure this.hash has a value before overriding default behavior
+        if(this.hash !== ""){
+            //Prevent default anchor click behavior
+            event.preventDefault();
 
-// // Get the navbar
-// var navbar = document.getElementById("mainNavbar");
+            //Store hash
+            var hash = this.hash; 
 
-// // Get the offset position of the navbar
-// var sticky = 700;
+            //Using jQuery's animate() method to add smooth page scroll
+            //The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+            $('html','body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                
+            //Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+            });
+        };
+    });
+});
